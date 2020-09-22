e2311effd65e7f775472185d2bc12c796ec84f7e
jessicarf18 <jessicarosalia678@gmail.com>
2020-09-22 18:10:15 -0300

    Criando arquivo js com o array de dados

---
+++ js/etapas.js
@@ -0,0 +1,71 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '11122',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {
+                        url: '11122.png',
+                        legenda: 'Vereador',
+                        small: false
+                    }
+                ]
+            },
+            {
+                numero: '90120',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {
+                        url: '90120.png',
+                        legenda: 'Vereador'
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '28',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {
+                        url: '28.jpg',
+                        legenda: 'Prefeito'
+                    },
+                    {
+                        url: '28_2.png',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            },
+            {
+                numero: '99',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {
+                        url: '99.png',
+                        legenda: 'Prefeito'
+                    },
+                    {
+                        url: '99_2.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            }
+        ]
+    }
+]
